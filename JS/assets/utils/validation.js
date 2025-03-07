667fd9c486181d6edd90618241d68e735a6f5faf
Tu-Trieu-Duong <tutrieuduong98@gmail.com>
2025-03-07 12:10:36 +0700

    style lại giao diện index cho dễ nhìn & fix lỗi cập nhật sản phẩm

--- JS/assets/utils/validation.js
+++ JS/assets/utils/validation.js
@@ -1,11 +1,19 @@
 export function kiemTraRong(value, selectorErr, id) {
-    //abc .trim()=>abc
-    if (value.trim() === "") {
-      document.getElementById(
-        selectorErr
-      ).innerHTML = `${id} không được bỏ trống`;
-      return false;
-    }
-    document.getElementById(selectorErr).innerHTML = "";
-    return true;
+  if (!value || typeof value !== "string") {
+    console.warn(`Lỗi: Giá trị của ${id} không hợp lệ (undefined hoặc null)`);
+    document.getElementById(
+      selectorErr
+    ).innerHTML = `${id} không được bỏ trống`;
+    return false;
   }
+
+  if (value.trim() === "") {
+    document.getElementById(
+      selectorErr
+    ).innerHTML = `${id} không được bỏ trống`;
+    return false;
+  }
+
+  document.getElementById(selectorErr).innerHTML = "";
+  return true;
+}
